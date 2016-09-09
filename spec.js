42a27712989d3757ad24e99c6cdee1d12ccf5c3b
Brian Smith <brian.smith.keith@gmail.com>
2016-09-09 15:53:28 -0400

    starting to validate prepack UI poc

--- spec.js
+++ spec.js
@@ -1,8 +1,25 @@
 // spec.js
-describe('Protractor Demo App', function() {
-  it('should have a title', function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
+var SearchPage = function() { 
+  this.departureDropDown = element(by.css("[name=departing]"));
+  // this.viewOnGitHubButton = this.centerStageButtons.all(by.css(".btn.btn-large")).get(0);
+  // this.downloadButton = this.centerStageButtons.element(by.css(".btn-primary"));
+  // this.designDocsButton = this.centerStageButtons.element(by.css(".btn-warning"));
+};
 
-    expect(browser.getTitle()).toEqual('Super Calculator');
+describe('Given I load the new prepack UI', function() {
+
+  beforeEach(function() {
+    browser.get('https://amatravel.test-wip.tstllc.net/prepack#/landing');
   });
+
+  it('should have a title', function() {
+    expect(browser.getTitle()).toEqual('Prepack');
+  });
+
+
+  it('should have a departure drop down', function(){
+      var searchPage = new SearchPage();
+    expect(searchPage.departureDropDown.isDisplayed()).toBe(true);
+  });
+       
 });
